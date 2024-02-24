f549f2c7d9c58b1ed9acc7d44e88cdb87a58eba0
usman <usmankhan.hse@gmail.com>
2024-02-24 23:00:05 +0400

    added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Routes, Route} from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
